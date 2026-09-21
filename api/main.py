89711ba2165d7005c48e8b69c9c6ee74c8d7e4c2
jilanispec <133861911+jilanispec@users.noreply.github.com>
2026-09-21 09:10:53 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1510,43 +1510,6 @@ def process_update(update):
 
 
 
-#===== PING =====
-
-@router.message(
-    Command("ping")
-)
-async def ping_command(
-    message: Message
-):
-
-    save_chat(
-        message
-    )
-
-
-    start = time.perf_counter()
-
-
-    sent = await message.reply(
-        "🏓 Pinging..."
-    )
-
-
-    latency = (
-        time.perf_counter()
-        - start
-    ) * 1000
-
-
-    await sent.edit_text(
-
-        f"🏓 <b>Pong!</b>\n\n"
-
-        "🟢 Status: Online\n"
-
-        f"⚡ Latency: "
-        f"<code>{latency:.2f} ms</code>"
-    )
 
 #===== WEBHOOK =====
 
